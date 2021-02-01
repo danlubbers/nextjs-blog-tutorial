152f71d2b6d56777ada4ffad7a49c9eaebf42947
Dan Lubbers <lubbers.dan@gmail.com>
2021-02-01 18:15:09 -0500

    Dynamic routes finished

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 
+import Link from "next/link";
+
 import { getSortedPostsData } from "../lib/posts";
 
 export const getStaticProps = async () => {
@@ -16,16 +18,18 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 classsName={utilStyles.headingLg}>Blog</h2>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => {
             return (
               <li className={utilStyles.listItem} key={id}>
-                {title}
+                <Link href={`/posts/${id}`}>
+                  <a>{title}</a>
+                </Link>
                 <br />
-                {id}
-                <br />
-                {date}
+                <small className={utilStyles.lightText}>
+                  <Date dateString={date} />
+                </small>
               </li>
             );
           })}
